5b2186f56d307a20f8fe18437e0e79ff7c696a6a
markheckmann (Mark Heckmann) <mark.heckmann@gmx.de>
2011-05-26 17:17:40 +0000

    changed intro message
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/openrepgrid@10 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/OpenRepGrid/R/zzz.R
+++ pkg/OpenRepGrid/R/zzz.R
@@ -2,11 +2,11 @@
 #
 .onLoad <- function(lib, pkg){
     packageStartupMessage(
-            "----------------------------------------------------------",
-          "\nOpenRepGrid: Tools for the analysis of repertory grid data",
-          "\nVersion", utils::packageDescription("OpenRepGrid", field="Version"), 
+            "-----------------------------------------------",
+          "\nOpenRepGrid Version ", utils::packageDescription("OpenRepGrid", field="Version"), 
+          "\nTools for the analysis of repertory grid data.",
           "\nFor an introduction visit: www.openrepgrid.org.",
-          "\n----------------------------------------------------------", 
+          "\n-----------------------------------------------", 
           appendLF = TRUE)
 
     # invisible object in workspace to store parameters (TODO)
